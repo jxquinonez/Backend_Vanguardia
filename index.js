5d4425ea6239bf3cba6b7edee3887faf836d57da
jxquinonez <77907758+jxquinonez@users.noreply.github.com>
2021-09-16 22:35:41 -0600

    Add Atlas DB connection

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-'use strict'
+"use strict";
 
-var mongoose = require('mongoose');
-var app = require('./app');
+var mongoose = require("mongoose");
+var app = require("./app");
 var port = 3900;
-
+require("dotenv").config();
 
 // mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Vanguardia', { useNewUrlParser:true})
-.then(() => {
-    console.log('Conexion a la BD bien!!');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => {
+  console.log("Conexion a la BD bien!!");
 
-    app.listen(port , () => {
-        console.log('servidor corriendo en http://localhost:'+port);
-    });
+  app.listen(port, () => {
+    console.log("servidor corriendo en http://localhost:" + port);
+  });
 });
-
